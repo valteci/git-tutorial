a7cca07943a7ce5a89c7d4a15d7051cca2f725e1
valteci <valtecijunior@gmail.com>
2024-10-12 17:35:14 -0300

    fazendo bagunça - resolvido!!!!

--- main.rb
+++ main.rb
@@ -4,7 +4,9 @@ require File.expand_path('loja_virtual/lib/loja_virtual')
 
 
 book  = Livro.new  'Maria' , '1234', 44, 23.99, :teste
-book2 = Livro.new  'Zeze', '9932', 100, 159.9, :teste
+
+book2 = Livro.new  'Zeze', '9932', 80, 159.9, :teste
+
 
 biblioteca = Biblioteca.new
 
