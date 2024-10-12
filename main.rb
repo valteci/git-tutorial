2068cc6f0b5c3b583262f15c234e399044adf40d
valteci <valtecijunior@gmail.com>
2024-10-12 09:12:44 -0300

    mudando o nome para Maria

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 
 require File.expand_path('loja_virtual/lib/loja_virtual')
 
-book  = Livro.new  'Guilherme' , '1234', 44, 23.99, :teste
+book  = Livro.new  'Maria' , '1234', 44, 23.99, :teste
 book2 = Livro.new  'joana', '9932', 65, 159.9, :teste
 
 biblioteca = Biblioteca.new
